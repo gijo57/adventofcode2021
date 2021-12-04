f162c904d1119dcb9b96916f4a03d640b2754c50
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-12-04 15:33:39 +0100

    day 3 answer 2

--- day_3/advent.py
+++ day_3/advent.py
@@ -21,23 +21,37 @@ def get_pwr_consumption(input, val_length):
 
 
 def get_life_support_rating(oxygen_input, co2_input, oxygen_start, co2_start):
-    print('ox', oxygen_input, len(oxygen_input), oxygen_start)
-    print('co', co2_input, len(co2_input), co2_start)
-
     oxygen_counts, co2_counts = [0, 0], [0, 0]
     for input in oxygen_input:
         oxygen_counts[int(input[oxygen_start])] += 1
+
     for input in co2_input:
         co2_counts[int(input[co2_start])] += 1
 
-    most_common_oxygen_bit = str(oxygen_counts.index(max(oxygen_counts)))
-    least_common_co2_bit = str(co2_counts.index(min(co2_counts)))
+    if (oxygen_counts[0] == oxygen_counts[1]):
+        most_common_oxygen_bit = str(1)
+    else:
+        most_common_oxygen_bit = str(oxygen_counts.index(max(oxygen_counts)))
 
-    oxygen_values = [x for x in oxygen_input if x[oxygen_start] == most_common_oxygen_bit]
-    co2_values = [x for x in co2_input if x[co2_start] == least_common_co2_bit]
+    if (co2_counts[0] == co2_counts[1]):
+        least_common_co2_bit = str(0)
+    else:
+        least_common_co2_bit = str(co2_counts.index(min(co2_counts)))
+
+    if (len(oxygen_input) > 1):
+        oxygen_values = [x for x in oxygen_input if x[oxygen_start] == most_common_oxygen_bit]
+    else:
+        oxygen_start -= 1
+        oxygen_values = oxygen_input
+
+    if (len(co2_input) > 1):
+        co2_values = [x for x in co2_input if x[co2_start] == least_common_co2_bit]
+    else:
+        co2_start -= 1
+        co2_values = co2_input
 
     if (len(oxygen_values) == 1 and len(co2_values) == 1): 
-        return oxygen_values, co2_values
+        return int(''.join(oxygen_values[0]), 2) * int(''.join(co2_values[0]), 2)
     else:
         return get_life_support_rating(oxygen_values, co2_values, oxygen_start + 1, co2_start + 1)
 
