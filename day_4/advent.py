8f8a51f224305523e9b355d0d8862a188b90f3c3
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-12-06 00:53:45 +0100

    day 4 solution 2

--- day_4/advent.py
+++ day_4/advent.py
@@ -6,26 +6,30 @@ with open('input.txt', 'r') as f:
     tables = [[[int(x) for x in row.split()] for row in t.split('\n')] for t in tables]
 
 
-def solve_1(numbers, tables):
+def get_score(numbers, table):
+    steps = 0
     for number in numbers:
-        for table in tables:
+        for row in table:
+            if number in row:
+                row[row.index(number)] = True
+            if all(x == True for x in row):
+                filtered_table = list(filter(lambda x: not isinstance(x, bool), [n for sublist in table for n in sublist]))
+                return sum(filtered_table) * number, steps
             for i in range(len(table[0])):
                 column = [row[i] for row in table]
                 if all(x == True for x in column):
                     filtered_table = list(filter(lambda x: not isinstance(x, bool), [n for sublist in table for n in sublist]))
-                    return sum(filtered_table) * number
-            for row in table:
-                if number in row:
-                    row[row.index(number)] = True
-                if all(x == True for x in row):
-                    filtered_table = list(filter(lambda x: not isinstance(x, bool), [n for sublist in table for n in sublist]))
-                    return sum(filtered_table) * number
+                    return sum(filtered_table) * number, steps
+        steps += 1
 
 
-def solve_2(numbers, tables):
+def solve(numbers, tables):
+    results = []
+    for table in tables:
+        results.append(get_score(numbers, table))
+    return results
 
-
-
-answer_1 = solve_1(numbers, tables)
-answer_2 = solve_2(numbers, tables)
+results = solve(numbers, tables)
+answer_1 = min(results, key = lambda x: x[1])[0]
+answer_2 = max(results, key = lambda x: x[1])[0]
 print(answer_1, answer_2)
