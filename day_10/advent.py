a6b61e00b57ad6474f26255f7ea5b7239a49547d
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-12-11 13:15:06 +0100

    day 10 answer 2

--- day_10/advent.py
+++ day_10/advent.py
@@ -1,3 +1,5 @@
+from statistics import median
+
 with open('index.txt', 'r') as f:
     lines = [x.strip() for x in f.readlines()]
 
@@ -15,6 +17,8 @@ scores = {
     ">": 25137
 }
 
+incomplete_lines = lines[:]
+
 
 def get_score():
     score = 0
@@ -27,10 +31,41 @@ def get_score():
             else:
                 opening_char = stack.pop()
                 if (char != pairs[opening_char]):
+                    incomplete_lines.pop(incomplete_lines.index(line))
                     score += scores[char]
                     break
     return score
 
 
+scores2 = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4,
+}
+
+
+def get_completion_score(line):
+    score = 0
+    stack = []
+    for char in line:
+        if (char in opening_chars):
+            stack.append(char)
+        else:
+            stack.pop()
+
+    for char in reversed(stack):
+        score = score * 5 + scores2[char]
+    return score
+
+
+def get_score2():
+    completion_scores = []
+    for line in incomplete_lines:
+        completion_scores.append(get_completion_score(line))
+    return median(completion_scores)
+
+
 answer1 = get_score()
-print(answer1)
+answer2 = get_score2()
+print(answer1, answer2)
