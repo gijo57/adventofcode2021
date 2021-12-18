f892fdbe1a2b77dc8f8586c43404df6f69ff12cb
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-12-18 18:57:50 +0200

    day 17 solution 1

---
+++ day_17/advent.py
@@ -0,0 +1,46 @@
+import re
+
+with open('input.txt', 'r') as f:
+    target_values = [int(x) for x in re.findall('(-\d*|\d*)', f.read()) if x != '']
+    x_bounds = target_values[:2]
+    y_bounds = target_values[2:]
+
+
+def run_step(velocity, location):
+    new_location = (location[0] + velocity[0], location[1] + velocity[1])
+    new_x_velocity = velocity[0] - 1 if velocity[0] > 0 else velocity[0] + 1 if velocity[0] < 0 else 0
+    new_velocity = (new_x_velocity, velocity[1] - 1)
+    return new_velocity, new_location
+
+
+def launch(init_velocity):
+    current_position = (0, 0)
+    current_velocity = init_velocity
+    highest_y = current_position[1]
+
+    while (current_position[1] >= min(y_bounds)):
+        current_velocity, current_position = run_step(current_velocity, current_position)
+        if (current_position[1] > highest_y):
+            highest_y = current_position[1]
+        legit_x = current_position[0] >= x_bounds[0] and current_position[0] <= x_bounds[1]
+        legit_y = current_position[1] >= y_bounds[0] and current_position[1] <= y_bounds[1]
+
+        if (legit_x and legit_y):
+            return highest_y
+
+
+def find_init_velocity():
+    highest_y = 0
+    for i in range(200):
+        for j in range(200):
+            y = launch((i, j))
+            print(y)
+            if (y):
+                if (y > highest_y):
+                    highest_y = y
+                elif (y < highest_y):
+                    break
+    return highest_y
+
+
+print(find_init_velocity())
